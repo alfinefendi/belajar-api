d8e34bda819340d7c90d032b3667f0bd69a4483e
alfinefendi <139411171+alfinefendi@users.noreply.github.com>
2024-05-02 14:43:26 +0000

    Add files via upload

---
+++ belajar-fetch-js/population-self-chart/population.js
@@ -0,0 +1,26 @@
+[
+    {
+        "year": 2015,
+        "population": 242.241352
+    },
+    {
+        "year": 2016,
+        "population": 282.214212
+    },
+    {
+        "year": 2017,
+        "population": 262.241352
+    },
+    {
+        "year": 2018,
+        "population": 292.214212
+    },
+    {
+        "year": 2019,
+        "population": 312.241352
+    },
+    {
+        "year": 2020,
+        "population": 222.214212
+    }
+]
